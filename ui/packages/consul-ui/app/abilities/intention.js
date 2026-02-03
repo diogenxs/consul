0845cf61b602b6ad2aabd9ccdef854cdd4f6b233
Diogenes <diogenes@telnyx.com>
2026-02-03 13:37:55 -0300

    INFRA-14010: Add hardcoded permission overrides to sidebar abilities
    Following the same pattern used by the KV ability, add canRead/canList/canWrite
    overrides that return true for:
    - zervice (Services)
    - node (Nodes)
    - intention (Intentions)
    - overview (Overview)
    
    This fixes the sidebar timing issue where abilities check permissions before
    the permission data is loaded asynchronously.

--- ui/packages/consul-ui/app/abilities/intention.js
+++ ui/packages/consul-ui/app/abilities/intention.js
@@ -10,12 +10,28 @@ import classic from 'ember-classic-decorator';
 export default class IntentionAbility extends BaseAbility {
   resource = 'intention';
 
+  /**/
+  // Temporarily revert to pre-1.10 UI functionality by overwriting frontend
+  // permissions. These are used to hide certain UI elements, but they are
+  // still enforced on the backend.
+  // This temporary measure should be removed again once https://github.com/hashicorp/consul/issues/11098
+  // has been resolved
+  get canRead() {
+    return true;
+  }
+
+  get canList() {
+    return true;
+  }
+  /**/
+
   get canWrite() {
     // Peered intentions aren't writable
     if (typeof this.item !== 'undefined' && typeof this.item.SourcePeer !== 'undefined') {
       return false;
     }
-    return super.canWrite && (typeof this.item === 'undefined' || !this.canViewCRD);
+    // Temporarily allowing all writes since permissions enforcement happens on the backend
+    return typeof this.item === 'undefined' || !this.canViewCRD;
   }
   get canViewCRD() {
     return typeof this.item !== 'undefined' && this.item.IsManagedByCRD;
