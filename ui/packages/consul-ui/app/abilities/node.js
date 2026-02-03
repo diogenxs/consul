12db68dad1eaef6da1319aedc9b2a20c3b9b64d4
Diogenes <diogenes@telnyx.com>
2026-02-03 13:37:55 -0300

    INFRA-14010: Add hardcoded permission overrides to sidebar abilities
    Following the same pattern used by the KV ability, add canRead/canList/canWrite
    overrides that return true for:
    - zervice (Services)
    - node (Nodes)
    - intention (Intentions)
    - overview (Overview)
    
    This fixes the sidebar timing issue where abilities check permissions before
    the permission data is loaded asynchronously.

--- ui/packages/consul-ui/app/abilities/node.js
+++ ui/packages/consul-ui/app/abilities/node.js
@@ -7,4 +7,23 @@ import BaseAbility from './base';
 
 export default class NodeAbility extends BaseAbility {
   resource = 'node';
+
+  /**/
+  // Temporarily revert to pre-1.10 UI functionality by overwriting frontend
+  // permissions. These are used to hide certain UI elements, but they are
+  // still enforced on the backend.
+  // This temporary measure should be removed again once https://github.com/hashicorp/consul/issues/11098
+  // has been resolved
+  get canRead() {
+    return true;
+  }
+
+  get canList() {
+    return true;
+  }
+
+  get canWrite() {
+    return true;
+  }
+  /**/
 }
--- ui/packages/consul-ui/app/abilities/overview.js
+++ ui/packages/consul-ui/app/abilities/overview.js
@@ -11,6 +11,18 @@ export default class OverviewAbility extends BaseAbility {
 
   resource = 'operator';
   segmented = false;
+
+  /**/
+  // Temporarily revert to pre-1.10 UI functionality by overwriting frontend
+  // permissions. These are used to hide certain UI elements, but they are
+  // still enforced on the backend.
+  // This temporary measure should be removed again once https://github.com/hashicorp/consul/issues/11098
+  // has been resolved
+  get canRead() {
+    return true;
+  }
+  /**/
+
   get canAccess() {
     return !this.env.var('CONSUL_HCP_ENABLED') && this.canRead;
   }
